4750e8ac21cf2358de2ab33819d2b72a6a21b9b7
Aayushktyagi <32107805+Aayushktyagi@users.noreply.github.com>
2019-02-24 20:17:09 +0530

    Add files via upload
    Added Visualization

--- autoencoder.py
+++ autoencoder.py
@@ -5,6 +5,7 @@ Autoencoder implementation using tensorflow
 
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 #load mnist Datasets
@@ -17,7 +18,7 @@ num_input = 784
 n_layer_1 = 256
 n_layer_2 = 128
 n_epochs = 10000
-n_learning_rate = 0.001
+n_learning_rate = 0.0001
 batch_size = 256
 display_step = 50
 
@@ -32,10 +33,10 @@ weights = {
     'decoder_h2':tf.Variable(tf.random_normal([n_layer_1,num_input]))
 }
 bias = {
-    'encoder_b1':tf.Variable(tf.random_normal([n_layer_1])),
-    'encoder_b2':tf.Variable(tf.random_normal([n_layer_2])),
-    'decoder_b1':tf.Variable(tf.random_normal([n_layer_1])),
-    'decoder_b2':tf.Variable(tf.random_normal([num_input]))
+    'encoder_b1':tf.Variable(tf.zeros([n_layer_1])),
+    'encoder_b2':tf.Variable(tf.zeros([n_layer_2])),
+    'decoder_b1':tf.Variable(tf.zeros([n_layer_1])),
+    'decoder_b2':tf.Variable(tf.zeros([num_input]))
 }
 def encoder(x , weights, bias):
     layer_1 = tf.add(tf.matmul(x,weights['encoder_h1']),bias['encoder_b1'])
@@ -75,3 +76,28 @@ with tf.Session() as sess:
         #Display batch_loss
         if i % display_step == 0 or i ==0:
             print("Epoch :{} , loss :{}".format(i,l))
+
+
+    #Starting testing
+    # encode and decode images from test image set and visualization of reconstruction
+    n = 4
+    canvas_orignal = np.empty((28*n,28*n))
+    canvas_recon = np.empty((28*n , 28*n))
+
+    for i in range(n):
+        #Get test dataset
+        batch_x , _ = mnist.test.next_batch(n)
+        g = sess.run(decoder_op , feed_dict = {X:batch_x})
+        #Display  orignal
+        for j in range(n):
+            canvas_orignal[i*28:(i+1)*28,j*28:(j+1)*28] = batch_x[j].reshape([28,28])
+
+    print("Orignal Images :")
+    plt.figure(figsize = (n,n))
+    plt.imshow(canvas_orignal,origin = "upper" , cmap = "gray")
+    plt.show()
+
+    print("Reconstructed Images :")
+    plt.figure(figsize = (n,n))
+    plt.imshow(canvas_recon,origin = "upper" , cmap = 'gray')
+    plt.show()
